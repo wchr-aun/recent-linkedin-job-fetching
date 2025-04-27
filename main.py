b189a895012545b70db28f2a4bf87c906dce7bc8
wchr-aun <wchr.aun@gmail.com>
2025-04-28 00:23:41 +0700

    fix: handle no jobs found

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import dataframeUtils
 import gSheetsUtils
 import jobUtils
 
-jobs = jobUtils.fetch_jobs()
-company_jobs = jobUtils.group_jobs_by_company_and_fetch_company_details(jobs)
-company_jobs = jobUtils.filter_and_sort_companies(company_jobs)
-dataframe = dataframeUtils.transform_to_df(company_jobs)
-gSheetsUtils.write_dataframe_to_google_sheets(dataframe)
+
+def main():
+    jobs = jobUtils.fetch_jobs()
+    if len(jobs) == 0:
+        print("No jobs found.")
+        return
+    company_jobs = jobUtils.group_jobs_by_company_and_fetch_company_details(jobs)
+    company_jobs = jobUtils.filter_and_sort_companies(company_jobs)
+    dataframe = dataframeUtils.transform_to_df(company_jobs)
+    gSheetsUtils.write_dataframe_to_google_sheets(dataframe)
+
+
+if __name__ == "__main__":
+    main()
